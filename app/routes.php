f6d6d0e349f6da7ec489d9a8c5ad7955fcb9e733
robboyland <boylandrobert@gmail.com>
2014-09-24 10:25:53 +0100

    add posts crud index & routes

--- app/routes.php
+++ app/routes.php
@@ -10,6 +10,7 @@
 | and give it the Closure to execute when that URI is requested.
 |
 */
+Route::resource('posts', 'PostsController');
 
 Route::get('/', function()
 {
--- app/views/posts/index.blade.php
+++ app/views/posts/index.blade.php
@@ -1,1 +1,8 @@
-/Users/robb/sandbox/blog/app/views/posts/index.blade.php
+@extends('layouts.master')
+
+@section('content')
+    <h1>Posts</h1>
+    @foreach($posts as $post)
+        <div>{{ $post->title }}</div>
+    @endforeach
+@stop

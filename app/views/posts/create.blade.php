1579732477acfb1d1692ccaa614423da53ae8adf
robboyland <boylandrobert@gmail.com>
2014-09-25 17:48:39 +0100

    add error messages posts create form

--- app/views/posts/create.blade.php
+++ app/views/posts/create.blade.php
@@ -5,16 +5,19 @@
             <div class="form-group">
                 {{ Form::label('title', 'title') }}
                 {{ Form::text('title', null, ['class' => 'form-control']) }}
+                {{ $errors->first('title') }}
             </div>
 
             <div class="form-group">
                 {{ Form::label('body', 'body') }}
                 {{ Form::text('body', null, ['class' => 'form-control']) }}
+                {{ $errors->first('body') }}
             </div>
 
             <div class="form-group">
                 {{ Form::label('categories', 'categories') }}
                 {{ Form::select('category_id', $categories, ['class' => 'form-control']) }}
+                {{ $errors->first('categories') }}
             </div>
 
                 {{ Form::hidden('user_id', 1) }}

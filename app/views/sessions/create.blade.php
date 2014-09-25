711cff6fb546a11db212021a0fc4e2fdd0b55a23
robboyland <boylandrobert@gmail.com>
2014-09-25 17:47:00 +0100

    add errors messages session create form

--- app/views/sessions/create.blade.php
+++ app/views/sessions/create.blade.php
@@ -5,12 +5,13 @@
             <div class="form-group">
                 {{ Form::label('email', 'email') }}
                 {{ Form::text('email', null, ['class' => 'form-control']) }}
+                {{ $errors->first('email') }}
             </div>
 
             <div class="form-group">
                 {{ Form::label('password', 'password') }}
                 {{ Form::password('password', ['class' => 'form-control']) }}
-
+                {{ $errors->first('password') }}
             </div>
 
             <div class="form-group">

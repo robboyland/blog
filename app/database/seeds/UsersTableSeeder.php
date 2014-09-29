cf313c591cc808f67e00c5597eba31578e7697c8
robboyland <boylandrobert@gmail.com>
2014-09-29 16:52:38 +0100

    refactor users table seeder

---
+++ app/database/seeds/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+// Composer: "fzaninotto/faker": "v1.3.0"
+use Faker\Factory as Faker;
+
+class UsersTableSeeder extends Seeder {
+
+	public function run()
+	{
+		$faker = Faker::create();
+
+        User::truncate();
+
+		foreach(range(1, 10) as $index)
+		{
+			User::create([
+                'name'      => $faker->name,
+                'email'     => 'me@example.com' . $index,
+                'password'  => Hash::make('pwd')
+			]);
+		}
+	}
+
+}

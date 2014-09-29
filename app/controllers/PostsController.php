12ca935fef12b32a8049b61267214b45f8fe6fa8
robboyland <boylandrobert@gmail.com>
2014-09-29 16:54:30 +0100

    add comments list to post.show page

--- app/controllers/PostsController.php
+++ app/controllers/PostsController.php
@@ -7,109 +7,111 @@ class PostsController extends \BaseController {
         $this->beforeFilter('auth', ['except' => 'show']);
     }
 
-	/**
-	 * Display a listing of posts
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-		// $posts = Post::all();
+    /**
+     * Display a listing of posts
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        // $posts = Post::all();
         $posts = Post::where('user_id', '=', Auth::user()->id)->get();
-		return View::make('posts.index', compact('posts'));
-	}
+        return View::make('posts.index', compact('posts'));
+    }
 
-	/**
-	 * Show the form for creating a new post
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
+    /**
+     * Show the form for creating a new post
+     *
+     * @return Response
+     */
+    public function create()
+    {
         $categories = DB::table('categories')->orderBy('name', 'asc')->lists('name','id');
 
-		return View::make('posts.create', compact('categories'));
-	}
+        return View::make('posts.create', compact('categories'));
+    }
 
-	/**
-	 * Store a newly created post in storage.
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-		$validator = Validator::make($data = Input::all(), ['title' => 'required', 'body' => 'required', 'user_id' => 'required', 'category_id' => 'required']);
+    /**
+     * Store a newly created post in storage.
+     *
+     * @return Response
+     */
+    public function store()
+    {
+        $validator = Validator::make($data = Input::all(), ['title' => 'required', 'body' => 'required', 'user_id' => 'required', 'category_id' => 'required']);
 
-		if ($validator->fails())
-		{
-			return Redirect::back()->withErrors($validator)->withInput();
-		}
+        if ($validator->fails())
+        {
+            return Redirect::back()->withErrors($validator)->withInput();
+        }
 
-		Post::create($data);
+        Post::create($data);
 
-		return Redirect::route('posts.index')->with('flash_message', 'New Post Created');
-	}
+        return Redirect::route('posts.index')->with('flash_message', 'New Post Created');
+    }
 
-	/**
-	 * Display the specified post.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		$post = Post::findOrFail($id);
+    /**
+     * Display the specified post.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        $post = Post::findOrFail($id);
 
-		return View::make('posts.show', compact('post'));
-	}
+        $comments = Comment::with('user')->where('post_id', '=', $post->id)->get();
 
-	/**
-	 * Show the form for editing the specified post.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		$post = Post::find($id);
+        return View::make('posts.show', compact('post', 'comments'));
+    }
+
+    /**
+     * Show the form for editing the specified post.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function edit($id)
+    {
+        $post = Post::find($id);
         $categories = DB::table('categories')->orderBy('name', 'asc')->lists('name','id');
 
-		return View::make('posts.edit', compact('post', 'categories'));
-	}
+        return View::make('posts.edit', compact('post', 'categories'));
+    }
 
-	/**
-	 * Update the specified post in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		$post = Post::findOrFail($id);
+    /**
+     * Update the specified post in storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function update($id)
+    {
+        $post = Post::findOrFail($id);
 
-		$validator = Validator::make($data = Input::all(), ['title' => 'required', 'body' => 'required', 'user_id' => 'required']);
+        $validator = Validator::make($data = Input::all(), ['title' => 'required', 'body' => 'required', 'user_id' => 'required']);
 
-		if ($validator->fails())
-		{
-			return Redirect::back()->withErrors($validator)->withInput();
-		}
+        if ($validator->fails())
+        {
+            return Redirect::back()->withErrors($validator)->withInput();
+        }
 
-		$post->update($data);
+        $post->update($data);
 
-		return Redirect::route('posts.index')->with('flash_message', 'Post Updated');
-	}
+        return Redirect::route('posts.index')->with('flash_message', 'Post Updated');
+    }
 
-	/**
-	 * Remove the specified post from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		Post::destroy($id);
+    /**
+     * Remove the specified post from storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        Post::destroy($id);
 
-		return Redirect::route('posts.index')->with('flash_message', 'Post Deleted');
-	}
+        return Redirect::route('posts.index')->with('flash_message', 'Post Deleted');
+    }
 
 }
--- app/views/posts/show.blade.php
+++ app/views/posts/show.blade.php
@@ -7,4 +7,32 @@
             <div class="article-copy">{{ $post->body }}</div>
         </div>
     </div>
+
+    <div id="row">
+        <div class="col-md-8 col-md-offset-2" id="post-comments">
+
+            {{ Form::open(['route' => 'comments.store'])}}
+                {{ Form::hidden('post_id', $post->id) }}
+                {{ Form::hidden('user_id', Auth::user()->id) }}
+                <div class="form-group">
+                    {{ Form::label('body', 'Comment:') }}
+                    {{ Form::textarea('body', null, ['class' => 'form-control']) }}
+                    {{ $errors->first('body') }}
+                </div>
+                <div class="form-group">
+                    {{ Form::submit('Create Post', ['class' => 'btn btn-primary']) }}
+                </div>
+            {{ Form::close() }}
+
+            @if (isset($comments))
+                <h2>Comments &nbsp;<span class="comment-count">{{ count($comments)}}</span></h2>
+                @foreach($comments as $comment)
+                    <section class="comments">
+                        <div class="comment-details">{{ $comment->created_at->format('Y m d') }} &middot; {{ $comment->user->name }}</div>
+                        <p>{{ $comment->body }}</p>
+                    </section>
+                @endforeach
+            @endif
+        </div>
+    </div>
 @stop

c739226d97702b5dcdd5cb6da1dad7ff75ff9a98
robboyland <boylandrobert@gmail.com>
2014-10-09 11:31:26 +0100

    add check for guest/user | show/hide comment form for post

--- app/views/posts/show.blade.php
+++ app/views/posts/show.blade.php
@@ -10,19 +10,20 @@
 
     <div id="row">
         <div class="col-md-8 col-md-offset-2" id="post-comments">
-
-            {{ Form::open(['route' => 'comments.store'])}}
-                {{ Form::hidden('post_id', $post->id) }}
-                {{ Form::hidden('user_id', Auth::user()->id) }}
-                <div class="form-group">
-                    {{ Form::label('body', 'Comment:') }}
-                    {{ Form::textarea('body', null, ['class' => 'form-control']) }}
-                    {{ $errors->first('body') }}
-                </div>
-                <div class="form-group">
-                    {{ Form::submit('Create Post', ['class' => 'btn btn-primary']) }}
-                </div>
-            {{ Form::close() }}
+            @if ( ! Auth::guest())
+                {{ Form::open(['route' => 'comments.store'])}}
+                    {{ Form::hidden('post_id', $post->id) }}
+                    {{ Form::hidden('user_id', Auth::user()->id) }}
+                    <div class="form-group">
+                        {{ Form::label('body', 'Comment:') }}
+                        {{ Form::textarea('body', null, ['class' => 'form-control']) }}
+                        {{ $errors->first('body') }}
+                    </div>
+                    <div class="form-group">
+                        {{ Form::submit('Create Post', ['class' => 'btn btn-primary']) }}
+                    </div>
+                {{ Form::close() }}
+            @endif
 
             @if (isset($comments))
                 <h2>Comments &nbsp;<span class="comment-count">{{ count($comments)}}</span></h2>

90ef69f98b510cbf1af2f40d26a1ec3d11d638e9
robboyland <boylandrobert@gmail.com>
2014-10-16 12:41:59 +0100

    add series id to posts table

---
+++ app/database/migrations/2014_10_16_113513_add_series_id_to_posts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSeriesIdToPostsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('posts', function(Blueprint $table)
+		{
+			$table->integer('series_id');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('posts', function(Blueprint $table)
+		{
+			$table->dropColumn('series_id');
+		});
+	}
+
+}

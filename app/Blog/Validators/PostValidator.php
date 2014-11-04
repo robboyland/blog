81a9114832be31f4afbf16591a8f28fb0c25b4a7
robboyland <boylandrobert@gmail.com>
2014-11-04 12:33:56 +0000

    edit Post validator validation rules

--- app/Blog/Validators/PostValidator.php
+++ app/Blog/Validators/PostValidator.php
@@ -5,6 +5,8 @@ class PostValidator extends Validator
     protected static $rules = [
         'title'   => 'required',
         'body'    => 'required',
-        'user_id' => 'required'
+        'user_id' => 'required',
+        'categories' => 'required',
+        'slug' => 'required'
     ];
 }

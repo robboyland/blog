b9205bf27822eccd53b308eb59a5f60675a49114
robboyland <boylandrobert@gmail.com>
2014-11-28 11:44:13 +0000

    add eloquent tag repository

---
+++ app/Blog/Repositories/Tag/EloquentTag.php
@@ -0,0 +1,56 @@
+<?php namespace Blog\Repositories\Tag;
+
+use Blog\Repositories\RepositoryAbstract;
+use Illuminate\Database\Eloquent\Model;
+
+class EloquentTag extends RepositoryAbstract implements TagInterface {
+
+    protected $tag;
+    protected $cache;
+
+    // Class expects an Eloquent model
+    public function __construct(Model $tag)
+    {
+        $this->tag = $tag;
+    }
+
+    /**
+     * Find existing tags or create if they don't exist
+     *
+     * @param  array $tags  Array of strings, each representing a tag
+     * @return array        Array or Arrayable collection of Tag objects
+     */
+    public function findOrCreate(array $tags)
+    {
+        $foundTags = $this->tag->whereIn('tag', $tags)->get();
+
+        $returnTags = array();
+
+        if( $foundTags )
+        {
+            foreach( $foundTags as $tag )
+            {
+                $pos = array_search($tag->tag, $tags);
+
+                // Add returned tags to array
+                if( $pos !== false )
+                {
+                    $returnTags[] = $tag;
+                    unset($tags[$pos]);
+                }
+            }
+        }
+
+        // Add remaining tags as new
+        foreach( $tags as $tag )
+        {
+            $returnTags[] = $this->tag->create(array(
+                                'tag' => $tag,
+                                'slug' => $this->slug($tag),
+                            ));
+        }
+
+        return $returnTags;
+    }
+
+}

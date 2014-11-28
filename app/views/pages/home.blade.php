7036ffcbb8c1f37651213a49f2687983d04e28dd
robboyland <boylandrobert@gmail.com>
2014-11-28 11:52:41 +0000

    edit home page article list add pagination links

--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -7,9 +7,16 @@
             @foreach($posts as $post)
                 <div>
                     <a href="/{{$post->slug}}">{{$post->title}}</a>
-                    <p>{{ $post->created_at->format('dS M y') }} | {{ $post->user->name }}</p>
+                    <p>{{ $post->created_at->format('dS M y') }} | {{ $post->user->name }} |
+                        <span class="category">category: {{ $post->category->name }}</span> |
+                        @foreach($post->tags as $tag)
+                            <span>{{ $tag->name }}</span>
+                        @endforeach
+                    </p>
                 </div>
             @endforeach
+
+            {{ $posts->links() }}
         </div>
         <div class="col-sm-3 col-sm-offset-1 blog-sidebar">
             <h2>categories</h2>

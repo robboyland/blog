7bd9c0ae35c6fec437bfa070f961268564c83382
robboyland <boylandrobert@gmail.com>
2014-11-29 13:24:13 +0000

    rename services dir to service + adjust namespaces

---
+++ app/Blog/Service/PostCreator.php
@@ -0,0 +1,27 @@
+<?php namespace Blog\Service;
+
+use Blog\Repositories\Article\ArticleInterface;
+use Blog\Validators\PostValidator;
+
+class PostCreator
+{
+    protected $validator;
+
+    protected $article;
+
+    public function __construct(PostValidator $validator, ArticleInterface $article)
+    {
+        $this->validator = $validator;
+        $this->article = $article;
+    }
+
+    public function make($attributes)
+    {
+        if ($this->validator->isValid($attributes))
+        {
+            return $this->article->create($attributes);
+        }
+
+        return $this->validator->getErrors();
+    }
+}
---
+++ app/Blog/Service/PostEditor.php
@@ -0,0 +1,23 @@
+<?php namespace Blog\Service;
+
+use Blog\Repositories\Article\ArticleInterface;
+use Blog\Validators\PostValidator;
+
+class PostEditor
+{
+    public function __construct(PostValidator $validator, ArticleInterface $article)
+    {
+        $this->validator = $validator;
+        $this->article = $article;
+    }
+
+    public function update($attributes)
+    {
+        if ($this->validator->isValid($attributes))
+        {
+            return $this->article->update($attributes);
+        }
+
+        return $this->validator->getErrors();
+    }
+}
